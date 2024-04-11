337945e80c66651ab5da194924481d4dc4141e30
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2024-04-11 14:54:41 +0000

    Update removing section title

--- src/navigation/vertical/index.ts
+++ src/navigation/vertical/index.ts
@@ -32,27 +32,6 @@ const navigation = (): VerticalNavItemsType => {
       path: '/portfolio-settings'
     },
     {
-      sectionTitle: 'Pages'
-    },
-    {
-      title: 'Login',
-      icon: Login,
-      path: '/pages/login',
-      openInNewTab: true
-    },
-    {
-      title: 'Register',
-      icon: AccountPlusOutline,
-      path: '/pages/register',
-      openInNewTab: true
-    },
-    {
-      title: 'Error',
-      icon: AlertCircleOutline,
-      path: '/pages/error',
-      openInNewTab: true
-    },
-    {
       sectionTitle: 'Customize'
     },
     {
