2c9e490f2978320ffbff7435015c6840df150a2a
hamza ouabiba <73914495+Hamza-ouabiba@users.noreply.github.com>
2024-04-21 20:24:06 +0100

    Revert "Merge pull request #5 from Tagmat101/Hamza"
    This reverts commit 3f9c8b99ed4a0909dd72bc32276ce2c14c933aaf, reversing
    changes made to 9de899e102213990a97ae5d0095c8e77b57d550b.

--- src/views/portfolio/ViewPortfolios.tsx
+++ src/views/portfolio/ViewPortfolios.tsx
@@ -1,79 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
-import CardPortfolio from '../cards/PortfolioCards/CardPortfolio';
-import { Categorie, PortfolioData } from 'src/utils/interfaces/int';
-import { GetAllPortfolios } from 'src/pages/api/PortfolioServices/Services';
-import { CircularProgress, TextField, MenuItem } from '@mui/material';
-import useDataFetching from 'src/@core/hooks/useFetchingData';
-import { GetCategoriesPortActive } from 'src/pages/api/CategoriePortServices/Service';
+  import { ChangeEvent, MouseEvent, useState, SyntheticEvent, useEffect } from 'react'
+  import Grid from '@mui/material/Grid'
+  import Typography from '@mui/material/Typography'
+  import CardPortfolio from '../cards/PortfolioCards/CardPortfolio'
+  import { PortfolioData } from 'src/utils/interfaces/int'
+  import { GetAllPortfolios } from 'src/pages/api/PortfolioServices/Services'
+  import { CircularProgress } from '@mui/material'
 
-const ViewPortfolios = () => {
-  const { data: portfolioData, loading: portfolioLoading, error: portfolioError } = useDataFetching<PortfolioData[]>(GetAllPortfolios);
-  const { data: categoryData, loading: categoryLoading, error: categoryError } = useDataFetching<Categorie[]>(GetCategoriesPortActive);
-  const [categoryFilter, setCategoryFilter] = useState('');
-  const [colorFilter, setColorFilter] = useState('');
-  const [searchTerm, setSearchTerm] = useState('');
+  const ViewPortfolios = () => {
+    const [data, setData] = useState<PortfolioData[]>([])
+    const [loading, setLoading] = useState(false)
 
-  const filteredData = portfolioData?.filter(portfolio => {
-    const categoryMatch = !categoryFilter || portfolio.categorie.name === categoryFilter;
-    const colorMatch = !colorFilter || portfolio.color === colorFilter;
-    const searchTermMatch = !searchTerm || portfolio.name.toLowerCase().includes(searchTerm.toLowerCase());
-    return categoryMatch && colorMatch && searchTermMatch;
-  }) || [];
-
-  return (
-    <Grid container spacing={6} padding={10}>
-      <Grid item xs={12} sx={{ paddingBottom: 4 }}>
-        <Typography variant='h5'>View all</Typography>
-      </Grid>
-      <Grid item xs={12} sx={{ paddingBottom: 2 }}>
-        <TextField
-          label="Search"
-          variant="outlined"
-          fullWidth
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-        />
-      </Grid>
-      <Grid item xs={12} sx={{ paddingBottom: 2 }}>
-        <TextField
-          select
-          label="Filter by category"
-          variant="outlined"
-          fullWidth
-          value={categoryFilter}
-          onChange={(e) => setCategoryFilter(e.target.value)}
-        >
-          <MenuItem value="">All</MenuItem>
-          {
-            categoryData && categoryData.map((category:Categorie) => {
-                return(
-                  <MenuItem value={category.name}>{category.name}</MenuItem>
-                )
-            })
+    useEffect(() => {
+      let isMounted = true; // Track whether the component is mounted
+    
+      async function getData() {
+        setLoading(true);
+        try {
+          const responsePortfolios = await GetAllPortfolios();
+          if (isMounted) {
+            setData(responsePortfolios);
+            setLoading(false);
           }
-        </TextField>
-      </Grid>
-      {portfolioLoading || categoryLoading ? (
-        <CircularProgress style={{ marginLeft: '10px' }} size={24} />
-      ) : portfolioError || categoryError ? (
-        <Typography variant="body1">Error: {portfolioError?.message || categoryError?.message}</Typography>
-      ) : (
-        <>
-          {filteredData.length > 0 ? (
-            filteredData.map((portfolio, index) => (
-              <Grid key={index} item xs={12} sm={6} md={3}>
-                <CardPortfolio portfolio={portfolio} />
-              </Grid>
-            ))
-          ) : (
-            <Typography variant="body1">No portfolios match the selected filters.</Typography>
-          )}
-        </>
-      )}
-    </Grid>
-  );
-}
+        } catch (error) {
+          console.error(error);
+          setLoading(false);
+        }
+      }
+    
+      getData();
+    
+      return () => {
+        isMounted = false;
+      };
+    }, []);
+    
 
-export default ViewPortfolios;
+    return (
+      <Grid container spacing={6} padding={10}>
+        <Grid item xs={12} sx={{ paddingBottom: 4 }}>
+          <Typography variant='h5'>View all</Typography>
+        </Grid>
+          {loading ? (
+            <CircularProgress style={{marginLeft: '10px'}} size={24} />
+          ) : (
+            <>
+              {data.length > 0 &&
+                data.map((portfolio, index) => (
+                  <Grid key={index} item xs={12} sm={6} md={3}>
+                    <CardPortfolio portfolio={portfolio} />
+                  </Grid>
+                ))}
+            </>
+          )}
+        
+      </Grid>
+    )
+  }
+
+  export default ViewPortfolios
